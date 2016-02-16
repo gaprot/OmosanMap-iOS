b8556eddea062b964f1b0627662bbe690c41d6f4
yuichi.kobayashi <yuichi.kobayashi@up-frontier.jp>
2016-02-16 18:27:20 +0900

    KMLを取得する試験実装

--- OmosanMaps/ViewController.swift
+++ OmosanMaps/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SSZipArchive
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loadData(sender: AnyObject) {
+        let directoryURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        var localURL: NSURL?
+        Alamofire
+            .download(
+                .GET,
+                "https://www.google.com/maps/d/u/0/kml?mid=zmkzjAlquG4s.k9j-gW9GbmCQ",
+                destination: { (temporaryURL, response) in
+                    let pathComponent = response.suggestedFilename
+            
+                    localURL = directoryURL.URLByAppendingPathComponent(pathComponent!)
+                    return localURL!
+        }).response{ (request, response, data, error) in
+            if let localPath = localURL?.path {
+                let kmlDirPath = localPath.stringByReplacingOccurrencesOfString(".kmz", withString: "")
+                if SSZipArchive.unzipFileAtPath(localPath, toDestination: kmlDirPath) {
+                    let kmlPath = kmlDirPath.stringByAppendingString("/doc.kml")
+                    if let kml = try? String(contentsOfFile: kmlPath, encoding: NSUTF8StringEncoding) {
+                        print(kml)
+                    }
+                }
+            }
+        }
+    }
 }
 
